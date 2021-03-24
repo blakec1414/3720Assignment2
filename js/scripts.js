d97b778cbcc8bdf451f0b95ca9208578c8034ff4
blakec1414 <blake.cullum@uleth.ca>
2021-03-24 08:01:46 -0700

    finish

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,15 @@
 
 var xhr = new XMLHttpRequest();
 //var url = "https://api.nasa.gov/planetary/apod?api_key=G1ToqzZncoUNO42lrAAV600DZyurcNWPk4piOI5J"
-var url2 = " https://www.poemist.com/api/v1/randompoems"
-var url = "https://binaryjazz.us/wp-json/genrenator/v1/genre/"
-
-xhr.open("GET", url, true);
-
-xhr.send(null);
-
-xhr.onload = function(){
-
-	if(xhr.status == 200){
-		//document.write(xhr.responseText)
-
-		var genreData = JSON.parse(xhr.responseText)
-
-		//console.log(genreData)
 
 
+$("#randomize").on("click",function(){
+clearAPI()
+callAPI()
 
-		//var newHeader = $("<h2>")
-		//newHeader.attr(genreData)
-		//$("body").prepend(newHeader)
+})
 
-		$("body").prepend("<h2>" + genreData + "</h2>")
-
-
-	}
-}
+var url2 = "https://binaryjazz.us/wp-json/genrenator/v1/genre/"
 
 xhr.open("GET", url2, true);
 
@@ -36,21 +18,108 @@ xhr.send(null);
 xhr.onload = function(){
 
 	if(xhr.status == 200){
-		//document.write(xhr.responseText)
-
-		var poemData = JSON.parse(xhr.responseText)
-
-		//console.log(poemData[0].content)
 
 
+		
 
-		//var newHeader = $("<h2>")
-		//newHeader.attr(genreData)
-		//$("body").prepend(newHeader)
+		var genreData = JSON.parse(xhr.responseText)
 
-		$("body").prepend("<h3>" + poemData[0].content + "</h3>")
+
+
+
+
+
+		$("p").append("<h2>" + genreData + "</h2>")
 
 
 	}
 }
 
+var xhr2 = new XMLHttpRequest();
+var url = " https://www.poemist.com/api/v1/randompoems"
+
+xhr2.open("GET", url, true);
+
+xhr2.send(null);
+
+xhr2.onload = function(){
+
+	if(xhr2.status == 200){
+
+
+		var poemData = JSON.parse(xhr2.responseText)
+
+		console.log(poemData)
+
+
+
+
+		$("p").append("<h3>" + poemData[0].title + "</h3>")
+		$("p").append("<h3>" + poemData[0].poet.name + "</h3>")
+		$("p").append("<h3>" + poemData[0].content + "</h3>")
+
+
+
+	}
+}
+
+function callAPI() {
+
+var url2 = "https://binaryjazz.us/wp-json/genrenator/v1/genre/"
+
+xhr.open("GET", url2, true);
+
+xhr.send(null);
+
+xhr.onload = function(){
+
+	if(xhr.status == 200){
+
+
+		
+
+		var genreData = JSON.parse(xhr.responseText)
+
+
+
+
+
+
+		$("p").append("<h2>" + genreData + "</h2>")
+
+
+	}
+}
+
+var xhr2 = new XMLHttpRequest();
+var url = " https://www.poemist.com/api/v1/randompoems"
+
+xhr2.open("GET", url, true);
+
+xhr2.send(null);
+
+xhr2.onload = function(){
+
+	if(xhr2.status == 200){
+
+
+		var poemData = JSON.parse(xhr2.responseText)
+
+		console.log(poemData)
+
+
+
+
+		$("p").append("<h3>" + poemData[0].title + "</h3>")
+		$("p").append("<h3>" + poemData[0].poet.name + "</h3>")
+		$("p").append("<h3>" + poemData[0].content + "</h3>")
+
+
+
+	}
+}
+
+
+}
+
+function clearAPI() {$("p").empty()}
